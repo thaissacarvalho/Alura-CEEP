0caed0245af77aae208f37833cf68ea859de7659
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2023-06-26 11:33:36 -0300

    DOM concluido

---
+++ README.md
@@ -0,0 +1,16 @@
+# CEEP - Manipulando o DOM
+
+Nesse projeto, é ensinado a fazer uma tarefa de lista dinâmica.
+
+Foi abordado o assunto:
+- querySelector;
+- Data Attributes;
+- addEventListener;
+- Value;
+- innerHTML;
+- Create Element;
+- remove;
+- apeenchild;
+etc...
+
+Finalizado em 26/06/2023.
---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+import BotaoConcluir from "./Componentes/concluiTarefa.js";
+import BotaoDeleta from "./Componentes/deletaTarefa.js";
+
+const criarTarefa = (evento) => {
+    evento.preventDefault();
+
+    const lista = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+
+    tarefa.appendChild(BotaoConcluir());
+    tarefa.appendChild(BotaoDeleta());
+    lista.appendChild(tarefa);
+    input.value = " ";
+}
+
+const novaTarefa = document.querySelector('[data-form-button]');
+
+novaTarefa.addEventListener('click', criarTarefa);
